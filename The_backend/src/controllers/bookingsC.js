6ac18ad33375faf4fdc974ed32ce916de540727a
Ishara Gold <isharagold6@gmail.com>
2025-11-20 21:59:11 +0200

    modified:   The_backend/src/controllers/bookingsC.js 	modified:   The_backend/src/models/Booking.js

--- The_backend/src/controllers/bookingsC.js
+++ The_backend/src/controllers/bookingsC.js
@@ -6,7 +6,9 @@ export const createBooking = async (req, res, next) =>{
         const {product: productId, quantity} = req. body;
         const product = await Product.findById(productId);
         if(!product) res.status(404).json({message: "Product not found"});
+        
         }
     catch(err){
         res.status(500).json({message: "server error"});
     }};
+   
--- The_backend/src/models/Booking.js
+++ The_backend/src/models/Booking.js
@@ -4,7 +4,7 @@ const bookingSchema = new mongoose.Schema(
     {
     user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
     product: {type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true},
-    quantity: {type: Number, default: 0, min: 0},
+    quantity: {type: Number, default: 0, min: 1},
     totalPrice: {type: Number, default: 0},
     status: {type: String, enum: ['pending', 'confirmed', 'canceled'], default: 'pending'},
 }, {timestamps: true
